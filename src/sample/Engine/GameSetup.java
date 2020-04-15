f162946246ba87b23b01c3ae8254239b7bff7f8a
Aslan2142 <Aslan2142@gmail.com>
2020-04-15 03:28:14 +0200

    Cleaned GameSetup

--- src/sample/Engine/GameSetup.java
+++ src/sample/Engine/GameSetup.java
@@ -4,10 +4,10 @@ import sample.Engine.Core.GameScene;
 
 public class GameSetup {
 
-    public final String gameName = "Pacman 3D";
-    public final int majorVersion = 0;
-    public final int minorVersion = 9;
-    public final int patchVersion = 9;
+    public final String gameName = "Game";
+    public final int majorVersion = 1;
+    public final int minorVersion = 0;
+    public final int patchVersion = 0;
     public final double targetFramerate = 60;
     public final int defaultResolutionX = 1280;
     public final int defaultResolutionY = 720;
